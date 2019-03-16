20db8f5a3c03b5a937fb968aa20ab0879cf7d3db
“Young <hogusong1@gmail.com>
2019-03-16 16:48:54 -0400

    create data models and auth service. 2

--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { USER, AUTHDATA } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private user: USER;
+
+
   constructor() { }
+
+  registerUser(authData: AUTHDATA) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 100000).toString()
+    };
+  }
+
+  login(authData: AUTHDATA) {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 100000).toString()
+    };
+  }
+
+  logouit() {
+    this.user = null;
+  }
+
+  getUser() {
+    return { ...this.user };
+  }
+
+  isAuth() {
+    return this.user != null;
+  }
 }
