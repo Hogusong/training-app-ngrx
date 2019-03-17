5dc352e04cffdd0c48937c95e1794aa71b6cd7a1
“Young <hogusong1@gmail.com>
2019-03-16 22:35:13 -0400

    implement 'AuthGuard' for routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { TrainingComponent } from './training/training.component';
+import { AuthGuard } from './auth/auth-guard';
 
 const appRoutes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [ AuthGuard ] },
   { path: '**', component: WelcomeComponent }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(appRoutes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  providers: [ AuthGuard ]
 })
 export class AppRoutingModule { }
