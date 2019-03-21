dab3079a780ff825836500a8b0a3f2dd0bf234c3
“Young <hogusong1@gmail.com>
2019-03-21 17:45:39 -0400

    add 'canLoad' in 'auth-guard' to use with lazy loader.

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,11 +1,11 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 
 import { AuthService } from "../providers/auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanLoad {
 
   authStatus = false;
 
@@ -15,6 +15,13 @@ export class AuthGuard implements CanActivate{
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): boolean | 
               Observable<boolean> | Promise<boolean> {
+    this.authStatus = this.authService.getAuthStatus();
+    if (this.authStatus) {  return true;  }
+    this.router.navigate(['/auth/login']);
+  }
+
+  // using with lazy loader
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
     this.authStatus = this.authService.getAuthStatus();
     if (this.authStatus) {  return true;  }
     this.router.navigate(['/auth/login']);
